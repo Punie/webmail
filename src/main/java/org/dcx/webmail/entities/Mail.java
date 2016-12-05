146ff52bd455d8e4ec358f0be4225cf8308d9ea9
Hugo Saracino <hugo.saracino@gmail.com>
2016-12-05 17:08:38 +0100

    Removed unnecessary imports

--- src/main/java/org/dcx/webmail/entities/Mail.java
+++ src/main/java/org/dcx/webmail/entities/Mail.java
@@ -1,10 +1,5 @@
 package org.dcx.webmail.entities;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-
 import javax.persistence.*;
 import java.util.Date;
 import java.util.Set;
